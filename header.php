71391a13611135500087037cd0f77081e2891b86
Bartosz Lewinski <jabarti@wp.pl>
2015-04-25 22:21:38 +0200

    prepare

--- header.php
+++ header.php
@@ -4,9 +4,9 @@
 	<title><?php echo (isset($title) ? t($title)  : 'My Page'); ?></title>
 	<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
 	<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-        <script src="<?php echo FUNCTIONS2_PATH."functions.js";?>"></script>
-        <script src="<?php echo FUNCTIONS2_PATH."Skrypty.js";?>"></script>
-        <script src="<?php echo FUNCTIONS2_PATH."Skrypty_FormValidator.js";?>"></script>
+        <script type="text/javascript" src="<?php echo FUNCTIONS2_PATH."functions.js";?>"></script>
+        <script type="text/javascript" src="<?php echo FUNCTIONS2_PATH."Skrypty.js";?>"></script>
+        <script type="text/javascript" src="<?php echo FUNCTIONS2_PATH."Skrypty_FormValidator.js";?>"></script>
         <!--<script src="Includes/functions/functions.js"></script>-->
         <link rel="shortcut icon" href="Resources/img/favicon_no_euro.ico" type="image/x-icon"/><!---->
         <link rel="Stylesheet" type="text/css" href="../Translations/translation.css" />
@@ -31,4 +31,7 @@
         <script type="text/javascript"          src="../ExternSources/dropdown-check-list.1.4/lib/jquery-1.6.1.min.js"></script>
         <script type="text/javascript"          src="../ExternSources/dropdown-check-list.1.4/lib/jquery-ui-1.8.13.custom.min.js"></script>
         <script type="text/javascript"          src="../ExternSources/dropdown-check-list.1.4/lib/ui.dropdownchecklist-1.4-min.js"></script>    
+        
+        <script type="text/javascript" src="<?php echo FUNCTIONS2_PATH."dropdowns-enhancement.js";?>"></script>
+        <link rel="stylesheet" href="Resources/CSS/dropdowns-enhancement.css">
 </head>
---
+++ title.php
@@ -0,0 +1,17 @@
+<?php
+
+/****************************************************
+ * Project:     Svenska
+ * Filename:    title.php
+ * Encoding:    UTF-8
+ * Created:     2015-04-25
+ *
+ * Author       Bartosz M. Lewiński <jabarti@wp.pl>
+ ***************************************************/
+
+?>
+<head>
+    <title><?php echo (isset($title)?$title  : 'My Page'); ?></title>   
+</head>
+
+
